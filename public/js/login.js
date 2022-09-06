e465eb6427c07e4a39f3390c8782f840143aa106
Elizabeth Mutesi <elizabethmutesi@gmail.com>
2022-09-06 23:18:03 +0300

    completed login styling

--- public/js/login.js
+++ public/js/login.js
@@ -5,29 +5,29 @@ function validationForm(){
     // let phone = document.forms["RegForm"]["Telephone"];
     //let username = document.forms["RegForm"]["username"];
     
-    nameRegex = /^[A-Za-z]+$/
+    // nameRegex = /^[A-Za-z]+$/
     emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^()])[A-Za-z\d@#$!%*?&^()]{8,}$/
-    numberRegex = /^[0-9]*$/
+    // numberRegex = /^[0-9]*$/
 
     alphaNumeric = /^[A-Za-z0-9]+$/
 
    
-    if(username.value == ""){
-        //alert("Please enter your name");
-        username.style.border = "2px solid red";
-        username.innerHTML = "Please enter your name";
-        username.focus();
-        return false;
-    }
-    else { username.style.border = "2px solid green";
+    // if(username.value == ""){
+    //     //alert("Please enter your name");
+    //     username.style.border = "2px solid red";
+    //     username.innerHTML = "Please enter your name";
+    //     username.focus();
+    //     return false;
+    // }
+    // else { username.style.border = "2px solid green";
 
-    }
+    // }
 
     if(email.value == ""|| !emailRegex.test(email.value) ){
         //alert("Please enter your email");
         email.style.border = "2px solid red";
-        //email.focus();
+        email.focus();
         return false
      } else{ email.style.border = "2px solid green";
         
@@ -36,19 +36,19 @@ function validationForm(){
     if(password.value == "" || !passRegex.test(password.value)){
         //alert("Please enter valid password");
         password.style.border = "2px solid red";
-        //password.focus();
+        password.focus();
         return false;
     }
     else { password.style.border = "2px solid green";
 
     }
 
-    if(phone.value == "" || phone.value.length != 10){
-        //alert("Please enter your valid phone number");
-        phone.style.border = "2px solid red";
-        //phone.focus();
-        return false;
-    } else { phone.style.border = "2px solid green";
-}
+//     if(phone.value == "" || phone.value.length != 10){
+//         //alert("Please enter your valid phone number");
+//         phone.style.border = "2px solid red";
+//         //phone.focus();
+//         return false;
+//     } else { phone.style.border = "2px solid green";
+// }
 
 }
